c5893787ac496442e0b103427dc1334a65089d4b
Matthew Black <matthewallenblack@gmail.com>
2024-01-04 00:16:59 -0600

    remove comment since its info is now in README

--- src/components/App/Nav.jsx
+++ src/components/App/Nav.jsx
@@ -16,18 +16,7 @@ function Nav() {
           <NavLink to="/auth">login/register</NavLink>}
       </li>
       <li>
-        <NavLink
-          to="/about"
-          // Nifty extra NavLink powers if following a certain data querying paradigm:
-            // https://reactrouter.com/en/main/components/nav-link
-          // Otherwise, difference between Link and NavLink is that NavLink, by default,
-          // has an 'active' CSS class applied to it when a user is "at" that route.
-          // className={({ isActive, isPending }) =>
-          //   isPending ? "pending" : isActive ? "active" : ""
-          // }
-        >
-          about
-        </NavLink>
+        <NavLink to="/about">about</NavLink>
       </li>
       {user.id && 
         <li>
