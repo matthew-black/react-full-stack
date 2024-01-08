7f8f76e9b412de225d996f29b65ae320f47e492c
Matthew Black <matthewallenblack@gmail.com>
2024-01-08 15:39:39 -0600

    change about and home page content

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,29 +1,33 @@
-import viteLogo from '/vite.svg'
+import { useAuthContext } from '../../contexts/AuthContext.jsx'
 
 
 function AboutPage() {
+  const { user } = useAuthContext()
 
   return (
     <div>
       <h2>About Page:</h2>
-      <p>Here are some Vite logos:</p>
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <img src={viteLogo} alt="Vite Logo" />
-      <p>Did you know that Vite is pronounced <em>veet</em>? (<em>/vit/</em>)</p>
-      <p>Also, Vite is the French word for "quick."</p>
-      <p>That's about it for this about page.</p>
+      {
+        user.id ?
+          <>
+            <p>This is a blog! In fact, it's a nice blog! You are logged-in, so you can do all the things:</p>
+            <ul>
+              <li>Read any public blog post that strikes your fancy!</li>
+              <li>Leave comments on any public blog post!</li>
+              <li>Write a blog post! Optionally make it public!</li>
+              <li>Edit or delete any of your own blog posts!</li>
+            </ul>
+            <p>It is nice to be logged-in, eh?</p>
+          </> 
+                :
+          <>
+            <p>This is a blog! In fact, it's a nice blog! You aren't currently logged-in, so you can really only:</p>
+            <ul>
+              <li>Read any public blog post that strikes your fancy!</li>
+            </ul>
+            <p>If you want to do more than that, you should totally register and/or log in!</p>
+          </>
+      }
     </div>
   )
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,34 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { useAuthContext } from '../../contexts/AuthContext.jsx'
 
 
-
 function HomePage() {
-  const { user } = useAuthContext()
+  const [publicPosts, setPublicPosts] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: '/posts/public'
+    })
+  }, [])
 
   return (
     <div>
       <h2>Home Page:</h2>
-      {
-        user.id ?
-          <>
-            <p>This is a blog! In fact, it's a nice blog! You are logged-in, so you can do all the things:</p>
-            <ul>
-              <li>Read any public blog post that strikes your fancy!</li>
-              <li>Leave comments on any public blog post!</li>
-              <li>Write a blog post! Optionally make it public!</li>
-              <li>Edit or delete any of your own blog posts!</li>
-            </ul>
-            <p>It is nice to be logged-in, eh?</p>
-          </> 
-                :
-          <>
-            <p>This is a blog! In fact, it's a nice blog! You aren't currently logged-in, so you can really only:</p>
-            <ul>
-              <li>Read any public blog post that strikes your fancy!</li>
-            </ul>
-            <p>If you want to do more than that, you should totally register and/or log in!</p>
-          </>
-      }
+
+      <h3>Recent Public Blog Posts:</h3>
+
     </div>
   )
 }
